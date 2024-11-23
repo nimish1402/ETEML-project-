bfcd7755052df6cfe4128a5064957a21b2609d7a
nimish1402 <selotnimish1539@gmail.com>
2024-11-23 23:40:35 +0530

    folder updated

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+# this file is created to log all the execution detials in the text file 
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# os.getcwd => get current working directory 
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,  
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
